62d7092c0d5c68e19e0a4193399d961151a07470
alex.ai <alexanderemmanuel1719@gmail.com>
2022-08-03 19:51:53 +0000

    First commit Netflix Clone

---
+++ src/request.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+const API_KEY="d1355fd7f4f7e5952897efe996a953b4";
+
+const requests={
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_newtworks=213`,
+    fetchTopRated:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHerrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
